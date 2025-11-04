07e4c6ef539b1ba89f15ee32ab8278fadab1b525
Mikev <mikyboy686@gmail.com>
2025-11-04 09:15:48 +0100

    revert changes

--- src/main/java/nl/blitz/oop/math/Calculator.java
+++ src/main/java/nl/blitz/oop/math/Calculator.java
@@ -14,16 +14,14 @@ public class Calculator {
      * TODO: Return the sum of two ints.
      */
     public int add(int a, int b) {
-        // throw new UnsupportedOperationException("TODO: implement add(int, int)");
-        return a + b;
+        throw new UnsupportedOperationException("TODO: implement add(int, int)");
     }
 
     /**
      * TODO: Return the sum of two doubles.
      */
     public double add(double a, double b) {
-        // throw new UnsupportedOperationException("TODO: implement add(double, double)");
-        return a + b;
+        throw new UnsupportedOperationException("TODO: implement add(double, double)");
     }
 
     /**
@@ -31,15 +29,7 @@ public class Calculator {
      * provided; if none, throw IllegalArgumentException.
      */
     public int add(int... nums) {
-        // throw new UnsupportedOperationException("TODO: implement add(int...)");
-        int total = 0;
-        if (nums.length < 2) {
-            throw new IllegalArgumentException("Requires more than 1 number");
-        }
-        for (int i : nums) {
-            total += i;
-        }
-        return total;
+        throw new UnsupportedOperationException("TODO: implement add(int...)");
     }
 }
 
--- src/main/java/nl/blitz/oop/math/Demo.java
+++ src/main/java/nl/blitz/oop/math/Demo.java
@@ -10,23 +10,8 @@ package nl.blitz.oop.math;
 public class Demo {
 
     public static void main(String[] args) {
-        // throw new UnsupportedOperationException("TODO: implement demo to exercise all add
-        // overloads");
-        Calculator calculator = new Calculator();
-
-        result_add_int_int = calculator.add(5, 5);
-        assert result1 = 10 : "The calculator is broken";
-        System.out.println(result1);
-
-        result_add_double_double = calculator.add(1.2, 1.2);
-        assert result2 = 2.4 : "The calculator is broken";
-        System.out.println(result2);
-
-        result_add_multi = calculator.add(1, 2, 3, 4, 6, 10)
-        assert result = 26 : "The calculator is broken";
-        System.out.println(result3);
-
-
+        throw new UnsupportedOperationException(
+                "TODO: implement demo to exercise all add overloads");
     }
 }
 
